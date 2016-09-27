1bbe3771d1d8d21d7c8b47d94b91c6378c73673f
WilliamIV <william.green.iv@gmail.com>
2016-09-27 19:20:23 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestWord
+{
+    namespace LongestWord
+    {
+        public class Program
+        {
+            public static void Main(string[] args)
+            {
+                string sentence = "Some line with text";
+                int sentLeng = sentence.Length;
+                string[] sent = sentence.Split(' ');
+                string longWord = "";
+                foreach (string word in sent)
+                {
+                    //For each word in the sentence array
+                    //count the characters chars in each word
+                    //determine which has the most chars
+
+                    int wordLength = word.Length;
+                    int longWordLength = longWord.Length;
+                    if (wordLength > longWordLength)
+                    {
+                        longWordLength = wordLength;
+                        longWord = word;
+                    }
+                }
+                Console.WriteLine(longWord);
+            }
+        }
+    }
+}
